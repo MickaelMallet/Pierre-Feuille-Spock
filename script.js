0370d0226d500a3a8cbdc2c2f6e620de7db9438c
MickaelMallet <mickaelmallet7@gmail.com>
2023-08-05 10:51:21 +0200

    setup player selection

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const playerScoreEl = document.getElementById('playerScore');
+const playerChoiceEl = document.getElementById('playerChoice'); 
+const computerScoreEl = document.getElementById('computerScore');
+const computerChoiceEl = document.getElementById('computerChoice');
+const resultText = document.getElementById('resultText'); 
+
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper'); 
+const playerScissors = document.getElementById('playerScissors'); 
+const playerLizard = document.getElementById('playerLizard'); 
+const playerSpock = document.getElementById('playerSpock'); 
+
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper'); 
+const computerScissors = document.getElementById('computerScissors'); 
+const computerLizard = document.getElementById('computerLizard'); 
+const computerSpock = document.getElementById('computerSpock'); 
+
+const allGameIcons = document.querySelectorAll('.far');
+
 
 
 const choices = {
@@ -7,4 +27,34 @@ const choices = {
   lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
+
+// Passing player selection value and styling icons
+
+function select(playerChoice) {
+  //Add selected styling &player choice 
+  switch (playerChoice) {
+    case 'rock' : 
+    playerRock.classList.add('selected');
+    playerChoiceEl.textContent = ' --- Rock';
+    break;
+    case 'paper' : 
+    playerRock.classList.add('selected');
+    playerChoiceEl.textContent = ' --- Paper';
+    break;
+    case 'scissors' : 
+    playerRock.classList.add('selected');
+    playerChoiceEl.textContent = ' --- Scissors';
+    break;
+    case 'lizard' : 
+    playerRock.classList.add('selected');
+    playerChoiceEl.textContent = ' --- Lizard';
+    break;
+    case 'spock' : 
+    playerRock.classList.add('selected');
+    playerChoiceEl.textContent = ' --- Spock';
+    break;
+    default: 
+    break;
+  }
+}
 
